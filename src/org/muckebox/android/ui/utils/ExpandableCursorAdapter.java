dc12c1cf4db7844744a869bed2dfb719b5c64216
Karsten Patzwaldt <karsten@patzwaldt.org>
2013-07-17 00:16:05 +0900

    Remove unused import.

--- src/org/muckebox/android/ui/utils/ExpandableCursorAdapter.java
+++ src/org/muckebox/android/ui/utils/ExpandableCursorAdapter.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.View.MeasureSpec;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
