c979913c420cf95f95253c3a8a11faa6fe866b6b
Karsten Patzwaldt <karsten@patzwaldt.org>
2013-08-03 20:26:19 +0900

    Make the drawer a bit more beautiful.

--- src/org/muckebox/android/ui/activity/BrowseActivity.java
+++ src/org/muckebox/android/ui/activity/BrowseActivity.java
@@ -33,12 +33,16 @@ import android.app.FragmentTransaction;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class BrowseActivity extends Activity
     implements AlbumListFragment.OnAlbumSelectedListener {
@@ -52,6 +56,53 @@ public class BrowseActivity extends Activity
     private DrawerLayout mDrawer;
     
     private ActionBarDrawerToggle mDrawerToggle;
+    
+    private static class DrawerEntry {
+        public DrawerEntry(int newIconId, int newTextId) {
+            iconId = newIconId;
+            textId = newTextId;
+        }
+        int iconId;
+        int textId;
+    }
+    
+    private static final DrawerEntry DRAWER_BROWSE_ENTRIES[] = new DrawerEntry[] {
+        new DrawerEntry(R.drawable.social_group, R.string.title_artists),
+        new DrawerEntry(R.drawable.collections_collection, R.string.title_albums)
+    };
+    
+    private static final DrawerEntry DRAWER_OTHER_ENTRIES[] = new DrawerEntry[] {
+        new DrawerEntry(R.drawable.av_download, R.string.title_activity_downloads),
+        new DrawerEntry(R.drawable.action_settings, R.string.title_activity_settings)
+    };
+    
+    private class DrawerListAdapter extends ArrayAdapter<DrawerEntry> {
+        DrawerEntry[] mEntries;
+        Activity mActivity;
+        
+        public DrawerListAdapter(Activity activity, DrawerEntry[] entries) {
+            super(activity, 0, entries);
+            
+            mEntries = entries;
+            mActivity = activity;
+        }
+        
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            if (view == null) {
+                LayoutInflater inflater = mActivity.getLayoutInflater();
+                view = inflater.inflate(R.layout.list_row_drawer, null);
+            }
+            
+            TextView textView = (TextView) view.findViewById(R.id.drawer_list_row_title);
+            ImageView iconView = (ImageView) view.findViewById(R.id.drawer_list_row_icon);
+            
+            textView.setText(mEntries[position].textId);
+            iconView.setImageResource(mEntries[position].iconId);
+            
+            return view;
+        }
+    }
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,19 +127,17 @@ public class BrowseActivity extends Activity
         }
 
         mBrowseList = (ListView) findViewById(R.id.drawer_browse_list);
-        mBrowseList.setAdapter(new ArrayAdapter<String>(this,
-            R.layout.list_row_drawer, R.id.drawer_list_row_title,
-            getResources().getStringArray(R.array.drawer_browse_entries)));
+        mBrowseList.setAdapter(new DrawerListAdapter(this, DRAWER_BROWSE_ENTRIES));
         
         mBrowseList.setOnItemClickListener(new ListView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Fragment target = null;
 
-                switch (position) {
-                case 0:
+                switch (DRAWER_BROWSE_ENTRIES[position].textId) {
+                case R.string.title_artists:
                     target = new ArtistListFragment();
                     break;
-                case 1:
+                case R.string.title_albums:
                     target = new AlbumListFragment();
                     break;
                 }
@@ -99,19 +148,17 @@ public class BrowseActivity extends Activity
         });
         
         mOtherList = (ListView) findViewById(R.id.drawer_other_list);
-        mOtherList.setAdapter(new ArrayAdapter<String>(this,
-            R.layout.list_row_drawer, R.id.drawer_list_row_title,
-            getResources().getStringArray(R.array.drawer_other_entries)));
+        mOtherList.setAdapter(new DrawerListAdapter(this, DRAWER_OTHER_ENTRIES));
         
         mOtherList.setOnItemClickListener(new ListView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Fragment target = null;
 
-                switch (position) {
-                case 0:
+                switch (DRAWER_OTHER_ENTRIES[position].textId) {
+                case R.string.title_activity_downloads:
                     target = new DownloadListFragment();
                     break;
-                case 1:
+                case R.string.title_activity_settings:
                     target = new SettingsFragment();
                     break;
                 }
